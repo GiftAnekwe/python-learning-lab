6bac5dd66a3293b3e2f98f1f9891beb103b3c65d
Gift Anekwe <giftanekwe@outlook.com>
2026-02-01 20:28:44 +0100

    Writing code to make common formula calculations

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-print("VS Code is working now?")
+#Pythagorean Theorem
+
+a = int(input("Give 'a' a number: "))
+b = int(input("Give 'b' a number: "))
+c = ((a**2 + b**2))**0.5
+print("Working with the Pythagorean theorem, c =", c)
+print('Another calc incoming.... hehehe')
+print('                                ')
+
+
+#Quadratic Formula
+A = int(input("Give 'A' a number: "))
+B = int(input("Give 'B' a number: "))
+C = int(input("Give 'C' a number: "))
+x_plus = (-B + ((B**2) - (4*A*C)))/2*A
+x_minus = (-B - ((B**2) - (4*A*C)))/2*A
+print('Using the Quadratic formula, X = ', x_plus, 'or', x_minus)
