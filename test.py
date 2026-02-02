d99bcb7f0264ad9d3677b95324089f1be5c817c3
Gift Anekwe <giftanekwe@outlook.com>
2026-02-02 09:35:39 +0100

    unnecessary spacing adjusted

--- test.py
+++ test.py
@@ -32,11 +32,9 @@ print("                                  ")
 butterflies = 10
 beetles = 12
 ladybugs = 20
-
 print('I caught ' + butterflies + ' 🦋 butterflies!')
 print('I caught ' + beetles + ' 🪲 beetles!')
 print('I caught ' + ladybug + ' 🐞 ladybugs!)
-
 print('I caught ' + str(total) + ' total bugs!'
 '''
 
