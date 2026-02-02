5286b19881efb7a3c23ddc97462b0502bcdd09ed
Gift Anekwe <giftanekwe@outlook.com>
2026-02-02 09:33:12 +0100

    edit code

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 #Pythagorean theorem
-a = int(input("Give 'a' a number: "))
-b = int(input("Give 'b' a number: "))
+a = float(input("Give 'a' a number: "))
+b = float(input("Give 'b' a number: "))
 c = ((a**2 + b**2))**0.5
 print("Working with the Pythagorean theorem, c =", c)
 print('                                ')
 
 print("Now, let's calculate another... hehehe")
 #Quadratic formula
-A = int(input("Give 'A' a number: "))
-B = int(input("Give 'B' a number: "))
-C = int(input("Give 'C' a number: "))
+A = float(input("Give 'A' a number: "))
+B = float(input("Give 'B' a number: "))
+C = float(input("Give 'C' a number: "))
 x_plus = (-B + ((B**2) - (4*A*C)))/2*A
 x_minus = (-B - ((B**2) - (4*A*C)))/2*A
 print('Using the Quadratic formula, X = ', x_plus, 'or', x_minus)
@@ -24,7 +24,7 @@ soles_usd = soles*0.30
 reais = float(input('How much worth of reais remains? '))
 reais_usd = reais*0.19
 USD_Total = int(pesos_usd + soles_usd + reais_usd)
-print("Your total amount of leftover cash in USD is approximately",USD_Total,"USD.")
+print("Your total amount of leftover cash is approximately",USD_Total,"USD.")
 print("                                  ")
 
 #Debugging demo
@@ -49,4 +49,3 @@ print('I caught ' + str(beetles) + ' 🪲 beetles!')
 print('I caught ' + str(ladybugs) + ' 🐞 ladybugs!')
 total = butterflies + beetles + ladybugs
 print('I caught ' + str(total) + ' bugs in total!')
-print("                                  ")
